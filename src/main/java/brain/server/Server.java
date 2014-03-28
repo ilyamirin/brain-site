b25071601e9e9e6ea56cf36a70e2193402937641
Ilya Mirin <ilya.mirin@gmail.com>
2014-03-28 11:28:21 +1100

    add velocity

--- src/main/java/brain/server/Server.java
+++ src/main/java/brain/server/Server.java
@@ -4,37 +4,48 @@ import static spark.Spark.*;
 
 import brain.brain.dao.QuestionDao;
 import brain.models.Question;
+import com.google.common.collect.Maps;
 import org.bson.types.ObjectId;
 import spark.*;
+import spark.template.velocity.VelocityRoute;
+
+import java.util.Map;
 
 public class Server {
+
+    //TODO:: добавить Велосити
+    //TODO:: доабвить авторизацию Facebook
+    //TODO:: не повторять человеку один и те же вопросы
+    //TODO:: добавить игру
+    //TODO:: сделать кнопки
+    //TODO:: добавить возможность посмотреть вопрос после ответа
+    //TODO:: добавить возможность редактировать вопросы
+    //TODO:: добавить возможность комментировать вопросы
 
     public static void main(String... args) {
         final QuestionDao questionDao = new QuestionDao();
         questionDao.init();
 
-        setPort(80);
+        setPort(8080);
 
-        get(new Route("/") {
+        get(new VelocityRoute("/") {
             @Override
             public Object handle(Request request, Response response) {
                 Question question = questionDao.getRandom();
-                String responseTemplate = "<html><body style='padding: 20px'><script>setTimeout(function() { document.location = '/answer/%s'}, 60000);</script><font size=\"80\"><h4>Вопрос: </h4><p>%s</p><a href='/answer/%s'>ОТВЕТ</a></font></body></html>";
-                return String.format(responseTemplate, question.getId(), question.getText(), question.getId());
+                Map<String, Object> model = Maps.newHashMap();
+                model.put("question", question);
+                return modelAndView(model, "main.html");
             }
         });
 
-        get(new Route("/answer/:questionId") {
+        get(new VelocityRoute("/answer/:questionId") {
             @Override
             public Object handle(Request request, Response response) {
                 String questionId = request.params(":questionId");
                 Question question = questionDao.get(new ObjectId(questionId));
-                String responseTemplate = "<html><body style='padding: 20px'><font size=\"80\"><h4>Ответ: </h4><p>%s</p>";
-                if (question.getComment() != null) {
-                    responseTemplate += "<h4>Комментарий: </h4><p>%s</p>";
-                }
-                responseTemplate +=  "<a href='/'>ИЩЩО</a></font></body></html>";
-                return String.format(responseTemplate, question.getAnswer(), question.getComment());
+                Map<String, Object> model = Maps.newHashMap();
+                model.put("question", question);
+                return modelAndView(model, "answer.html");
             }
         });
 
